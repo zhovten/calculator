c3fce3a03434d86fa86f54aca95c86589bdb1bab
zhovten <blckzhvtn@gmail.com>
2019-10-10 18:41:58 +0300

    123

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-document.write('Hello World from javascript!');
+const btn = document.querySelectorAll('.btn');
+let display = document.querySelector('.disp');
+let arrArgument = [];
+
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', f);
+}
+
+function f() {
+    if (this.value === "=") {
+        display.value = eval(arrArgument.join(''));
+    } else {
+        arrArgument.push(this.value);
+        display.value = arrArgument.join('');
+    }
+}
